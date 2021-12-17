eb79b03c76a7e5c6749e7fb421964655e88ad12f
Nico Van den Hooff <nicovandenhooff@gmail.com>
2021-12-16 21:59:44 -0800

    add day 8 part 1 solution and part 2 info

---
+++ problems/day-8-seven-segment-search/solution.py
@@ -0,0 +1,53 @@
+# Advent of Code 2021
+# Day 8: Part 1 and Part 2
+# Author: Nico Van den Hooff
+
+
+def read_data(path):
+    inputs = []
+    outputs = []
+
+    with open(path, "r") as f:
+        data = f.read().splitlines()
+
+    # split inputs and outputs
+    for i, string in enumerate(data):
+        data[i] = string.split(" | ")
+
+    # create lists of inputs and outputs
+    for sequence in data:
+        inputs.append(sequence[0].split())
+        outputs.append(sequence[1].split())
+
+    return inputs, outputs
+
+
+def part_1(outputs):
+    counts = 0
+    uniques = {2, 3, 4, 7}
+
+    for sequences in outputs:
+        for sequence in sequences:
+            if len(sequence) in uniques:
+                counts += 1
+
+    return counts
+
+
+def part_2(inputs, outputs):
+    pass
+
+
+def main(path):
+    pass
+    inputs, outputs = read_data(path)
+    solution1 = part_1(outputs)
+    # solution2 = part_2(data)
+
+    print(f"Part 1 Solution: {solution1}")
+    # print(f"Part 2 Solution: {solution2}")
+
+
+if __name__ == "__main__":
+    path = "problems/day-8-seven-segment-search/input.txt"
+    main(path)
